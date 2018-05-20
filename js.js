b478d5f09ddb900e46cc5fe6a3c3b1a07535c74d
misha skopenko <misha.skopenko@yandex.ru>
2018-05-20 23:25:54 +0300

    initial commit

---
+++ js.js
@@ -0,0 +1,6 @@
+function out(){
+var div;
+div=document.getElementById('out');
+div.outerHTML='<div class="one">super easy going freeride boards<br>based on the X-Cite Ride shape con-<br>cept with additional control and super<br></div>';
+
+}
